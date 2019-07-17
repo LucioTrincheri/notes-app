f91a0a4c424bd014ee0225a1699a50989b6db5fb
Lucio Trincheri <lucio4852071@gmail.com>
2019-07-17 17:11:18 -0300

    Initial commit

---
+++ src/components/AddTodo.js
@@ -0,0 +1,47 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import style from "../styles/index.module.css";
+
+class AddTodo extends Component {
+	state = {
+		title: ""
+	};
+
+	onSubmit = e => {
+		e.preventDefault();
+		this.props.addTodo(this.state.title, this.props.groupId);
+		this.setState({ title: "" });
+	};
+
+	onChange = e => this.setState({ [e.target.name]: e.target.value });
+
+	render() {
+		return (
+			<form className={style.addTodo} onSubmit={this.onSubmit}>
+				<input
+					type="text"
+					name="title"
+					style={{ flex: "10", padding: "5px" }}
+					placeholder="Add Note..."
+					value={this.state.title}
+					onChange={this.onChange}
+				/>
+				<input
+					type="submit"
+					value="Submit"
+					className="btn"
+					style={{ flex: "1" }}
+				/>
+			</form>
+		);
+	}
+}
+
+// PropTypes
+
+AddTodo.propTypes = {
+	groupId: PropTypes.number.isRequired,
+	addTodo: PropTypes.func.isRequired
+};
+
+export default AddTodo;
---
+++ src/components/AddTodoGroup.js
@@ -0,0 +1,42 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import style from "../styles/index.module.css";
+
+export class AddTodoGroup extends Component {
+	state = {
+		title: ""
+	};
+
+	onSubmit = e => {
+		e.preventDefault();
+		this.props.addTodoGroup(this.state.title);
+		this.setState({ title: "" });
+	};
+
+	onChange = e => this.setState({ [e.target.name]: e.target.value });
+	render() {
+		return (
+			<form className={style.addTodoGroup} onSubmit={this.onSubmit}>
+				<input
+					type="text"
+					name="title"
+					style={{ flex: "10", padding: "5px" }}
+					placeholder="Add Group..."
+					value={this.state.title}
+					onChange={this.onChange}
+				/>
+				<input
+					type="submit"
+					value="Submit"
+					className="btn"
+					style={{ flex: "1" }}
+				/>
+			</form>
+		);
+	}
+}
+
+AddTodoGroup.propTypes = {
+	addTodoGroup: PropTypes.func.isRequired
+};
+export default AddTodoGroup;
