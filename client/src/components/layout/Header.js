531339b8fa52a240792cd9dd72915ef0daa98bcb
Lucio Trincheri <lucio4852071@gmail.com>
2019-08-28 14:08:15 -0300

    Added page links

--- client/src/components/layout/Header.js
+++ client/src/components/layout/Header.js
@@ -1,31 +1,39 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Header() {
 	return (
 		<header style={headerStyle}>
 			<h1>TodoList</h1>
-			<Link style={linkStyle} to="/">
+			<Link style={linkStyle} to='/'>
 				Home
-			</Link>{" "}
-			|{" "}
-			<Link style={linkStyle} to="/about">
+			</Link>{' '}
+			|{' '}
+			<Link style={linkStyle} to='/about'>
 				About
+			</Link>{' '}
+			|{' '}
+			<Link style={linkStyle} to='/login'>
+				Login
+			</Link>{' '}
+			|{' '}
+			<Link style={linkStyle} to='/register'>
+				Register
 			</Link>
 		</header>
 	);
 }
 
 const headerStyle = {
-	background: "#333",
-	color: "#fff",
-	textAlign: "center",
-	padding: "10px"
+	background: '#333',
+	color: '#fff',
+	textAlign: 'center',
+	padding: '10px'
 };
 
 const linkStyle = {
-	color: "#fff",
-	textDecoration: "none"
+	color: '#fff',
+	textDecoration: 'none'
 };
 
 export default Header;
